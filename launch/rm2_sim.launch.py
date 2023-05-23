1767949e8930b5ab4830c8e8e23a86c5d9e95d82
estherag <e.aguado.glez@gmail.com>
2023-05-23 11:38:28 +0200

    add two robots in SDF

--- launch/rm2_sim.launch.py
+++ launch/rm2_sim.launch.py
@@ -29,7 +29,7 @@ def generate_launch_description():
     return LaunchDescription([
       DeclareLaunchArgument(
           'ign_args',
-            default_value=[os.path.join(pkg, 'worlds', 'cave_world.sdf')]),
+            default_value=[os.path.join(pkg, 'worlds', 'cave_world_robot.sdf')]),
         ign_gazebo,
         ign_bridge,
     ])
